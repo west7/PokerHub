6a90e32c11eece0815681be08a7e0f58a06f1aec
Guilherme Westphall <guilhermewestphall@gmail.com>
2024-10-18 22:09:59 -0300

    changing the interface

--- interfaces/player.interface.ts
+++ interfaces/player.interface.ts
@@ -1,14 +1,10 @@
-export interface Balance {
-    amount: number;
-    type: 'gain' | 'loss';
-    date: Date;
-}
+
 export interface Player {
     playerId: string;
     name: string;
     totalBalance: number;
     gamesPlayed: number;
     gamesWon: number;
-    lastTransaction: Balance | null;
+    lastTransaction: number;
     //lastGame: Match;
 }
